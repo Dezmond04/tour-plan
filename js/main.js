8073195d67a8c34587fdd146b94a4a302c6fea3c
Dezmond04 <Dezmond04@mail.ru>
2021-05-21 15:55:30 +0300

    create: map via api, fix: change layout for pp

--- js/main.js
+++ js/main.js
@@ -13,3 +13,35 @@ const swiper = new Swiper(".swiper-container", {
 	},
 	effect: "coverflow",
 });
+
+ymaps.ready(init);
+
+function init() {
+	var myMap = new ymaps.Map(
+		"map",
+		{
+			center: [32.089666, 34.77077],
+			zoom: 15,
+		},
+		{
+			searchControlProvider: "yandex#search",
+		},
+	);
+	myGeoObject = new ymaps.GeoObject({});
+	myMap.geoObjects.add(myGeoObject).add(
+		new ymaps.Placemark(
+			[32.089666, 34.77077],
+			{
+				balloonContentHeader: "Hilton Hotel",
+				balloonContentBody: "Hilton Hotel Элитный пляжный отель с рестораном",
+				balloonContentFooter:
+					"Официальный сайт: <a href='https://web-coup.ru/homeworks/tour-plan/'>https://web-coup.ru/homeworks/tour-plan/</a>",
+				hintContent: "Hilton Hotel",
+			},
+			{
+				preset: "islands#dotIcon",
+				iconColor: "#ff2b2b",
+			},
+		),
+	);
+}
