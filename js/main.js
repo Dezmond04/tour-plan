ff2d4edcfc317d5c0ab0a885fb3a388baa89ac6d
Dezmond04 <Dezmond04@mail.ru>
2021-05-29 02:36:34 +0300

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -1,74 +1,92 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
-	// Optional parameters
-	loop: true,
-
-	// Navigation arrows
-	navigation: {
-		nextEl: ".hotel-slider__button--next",
-		prevEl: ".hotel-slider__button--prev",
-	},
-	effect: "coverflow",
-});
-const reviewsSlider = new Swiper(".reviews-slider", {
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
-	// Optional parameters
-	loop: true,
-
-	// Navigation arrows
-	navigation: {
-		nextEl: ".reviews-slider__button--next",
-		prevEl: ".reviews-slider__button--prev",
-	},
-});
-
-$(".newsletter").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-	console.log("Клик по кнопке");
-	document
-		.querySelector(".navbar-bottom")
-		.classList.toggle("navbar-bottom--visible");
-});
-
-ymaps.ready(init);
-
-function init() {
-	var myMap = new ymaps.Map(
-		"map",
-		{
-			center: [32.089666, 34.77077],
-			zoom: 15,
+$(document).ready(function () {
+	const hotelSlider = new Swiper(".hotel-slider", {
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
 		},
-		{
-			searchControlProvider: "yandex#search",
+		// Optional parameters
+		loop: true,
+
+		// Navigation arrows
+		navigation: {
+			nextEl: ".hotel-slider__button--next",
+			prevEl: ".hotel-slider__button--prev",
 		},
-	);
+		effect: "coverflow",
+	});
+	const reviewsSlider = new Swiper(".reviews-slider", {
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+		},
+		// Optional parameters
+		loop: true,
 
-	myMap.behaviors.disable("scrollZoom");
+		// Navigation arrows
+		navigation: {
+			nextEl: ".reviews-slider__button--next",
+			prevEl: ".reviews-slider__button--prev",
+		},
+	});
 
-	myGeoObject = new ymaps.GeoObject({});
-	myMap.geoObjects.add(myGeoObject).add(
-		new ymaps.Placemark(
-			[32.089666, 34.77077],
+	$(".newsletter").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+
+	var menuButton = $(".menu-button");
+	menuButton.on("click", function () {
+		$(".navbar-bottom").toggleClass("navbar-bottom--visible");
+	});
+
+	ymaps.ready(init);
+
+	function init() {
+		var myMap = new ymaps.Map(
+			"map",
 			{
-				balloonContentHeader: "Hilton Hotel",
-				balloonContentBody: "Hilton Hotel Элитный пляжный отель с рестораном",
-				balloonContentFooter:
-					"Официальный сайт: <a href='https://web-coup.ru/homeworks/tour-plan/'>https://web-coup.ru/homeworks/tour-plan/</a>",
-				hintContent: "Hilton Hotel",
+				center: [32.089666, 34.77077],
+				zoom: 15,
 			},
 			{
-				preset: "islands#dotIcon",
-				iconColor: "#ff2b2b",
+				searchControlProvider: "yandex#search",
 			},
-		),
-	);
-}
+		);
+
+		myMap.behaviors.disable("scrollZoom");
+
+		myGeoObject = new ymaps.GeoObject({});
+		myMap.geoObjects.add(myGeoObject).add(
+			new ymaps.Placemark(
+				[32.089666, 34.77077],
+				{
+					balloonContentHeader: "Hilton Hotel",
+					balloonContentBody: "Hilton Hotel Элитный пляжный отель с рестораном",
+					balloonContentFooter:
+						"Официальный сайт: <a href='https://web-coup.ru/homeworks/tour-plan/'>https://web-coup.ru/homeworks/tour-plan/</a>",
+					hintContent: "Hilton Hotel",
+				},
+				{
+					preset: "islands#dotIcon",
+					iconColor: "#ff2b2b",
+				},
+			),
+		);
+	}
+
+	var modalButton = $("[data-toggle=modal]");
+	var closeModalButton = $(".modal__close");
+	modalButton.on("click", openModal);
+	closeModalButton.on("click", closeModal);
+
+	function openModal() {
+		var modalOverlay = $(".modal__overlay");
+		var modalDialog = $(".modal__dialog");
+		modalOverlay.addClass("modal__overlay--visible");
+		modalDialog.addClass("modal__dialog--visible");
+	}
+	function closeModal(event) {
+		event.preventDefault();
+		var modalOverlay = $(".modal__overlay");
+		var modalDialog = $(".modal__dialog");
+		modalOverlay.removeClass("modal__overlay--visible");
+		modalDialog.removeClass("modal__dialog--visible");
+	}
+});
---
+++ send.php
@@ -0,0 +1,97 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$user_email = $_POST['user_email'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+
+if(!isset($user_email)){
+    $body = "
+    <h2>Новое обращение</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон:</b> $phone<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+}
+else{
+    $body = " <h2>Вы подписались на рассылку новостей </h2>";
+}
+
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'karih040890@gmail.com'; // Логин на почте
+    $mail->Password   = '040890bujhm'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('karih040890@gmail.com', 'Игорь Карих'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    // isset - проверка на существование переменной 
+    // "Функция isset() возвращает true, если переменная существует и её значение не null"
+    if(!isset($user_email)){
+        $mail->addAddress('i.karih.home@gmail.com');
+    }else
+    {
+        $mail->addAddress($user_email);
+    }
+    
+    
+    // $mail->addAddress('i.pereyaslovskiy@gmail.com');  
+    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+//echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+
+
+if(isset($_SESSION)){
+    session_destroy();
+}
+
+session_start();
+
+if(isset($user_email)) {
+    $_SESSION['thankyou_title'] = 'Thank you!';
+    $_SESSION['thankyou_description'] = 'You subscribed to notifications';
+}else{
+    $_SESSION['thankyou_title'] = 'MESSAGE SENT';
+    $_SESSION['thankyou_description'] = 'Our manager will call you in 5 minutes';
+}
+
+
+header('Location: thankyou.php');
