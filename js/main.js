396dcd47822c3f72c6a22763fd0c4ffdd15ef59e
Dezmond04 <Dezmond04@mail.ru>
2021-05-30 14:29:11 +0300

    fix: optimization and fix bags

--- js/main.js
+++ js/main.js
@@ -40,41 +40,6 @@ $(document).ready(function () {
 		$(".navbar-bottom").toggleClass("navbar-bottom--visible");
 	});
 
-	ymaps.ready(init);
-
-	function init() {
-		var myMap = new ymaps.Map(
-			"map",
-			{
-				center: [32.089666, 34.77077],
-				zoom: 15,
-			},
-			{
-				searchControlProvider: "yandex#search",
-			},
-		);
-
-		myMap.behaviors.disable("scrollZoom");
-
-		myGeoObject = new ymaps.GeoObject({});
-		myMap.geoObjects.add(myGeoObject).add(
-			new ymaps.Placemark(
-				[32.089666, 34.77077],
-				{
-					balloonContentHeader: "Hilton Hotel",
-					balloonContentBody: "Hilton Hotel Элитный пляжный отель с рестораном",
-					balloonContentFooter:
-						"Официальный сайт: <a href='https://web-coup.ru/homeworks/tour-plan/'>https://web-coup.ru/homeworks/tour-plan/</a>",
-					hintContent: "Hilton Hotel",
-				},
-				{
-					preset: "islands#dotIcon",
-					iconColor: "#ff2b2b",
-				},
-			),
-		);
-	}
-
 	var modalButton = $("[data-toggle=modal]");
 	var closeModalButton = $(".modal__close");
 	modalButton.on("click", openModal);
